7c31287ce84b411a9796ebd8277bb2820894b255
Serg Minin <everestdon@ukr.net>
2019-06-08 15:48:24 +0300

    refactored lists

--- src/components/Blocks/FooterBlock/index.jsx
+++ src/components/Blocks/FooterBlock/index.jsx
@@ -8,6 +8,18 @@ const list01 = ["About", "Sportlight", "Contact"];
 const list02 = ["Scrapbook", "Resources", "Help & FAQ"];
 const list03 = ["Advertising", "Terms of use", "Privacy Policy"];
 
+const listMenu = element => {
+  return element.map((item, index) => {
+    return (
+      <li key={index} className={styles.menuList}>
+        <a href="localhost:3000" className={styles.menuLink}>
+          {item}
+        </a>
+      </li>
+    );
+  });
+};
+
 const FooterBlock = () => {
   return (
     <div className={styles.footerBlock}>
@@ -46,45 +58,15 @@ const FooterBlock = () => {
 
             <div className={styles.menu}>
               <div className={styles.menuGroup01}>
-                <ul className={styles.groupItems}>
-                  {list01.map((item, index) => {
-                    return (
-                      <li key={index} className={styles.menuList}>
-                        <a href="localhost:3000" className={styles.menuLink}>
-                          {item}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
+                <ul className={styles.groupItems}>{listMenu(list01)}</ul>
               </div>
 
               <div className={styles.menuGroup02}>
-                <ul className={styles.groupItems}>
-                  {list02.map((item, index) => {
-                    return (
-                      <li key={index} className={styles.menuList}>
-                        <a href="localhost:3000" className={styles.menuLink}>
-                          {item}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
+                <ul className={styles.groupItems}>{listMenu(list02)}</ul>
               </div>
 
               <div className={styles.menuGroup03}>
-                <ul className={styles.groupItems}>
-                  {list03.map((item, index) => {
-                    return (
-                      <li key={index} className={styles.menuList}>
-                        <a href="localhost:3000" className={styles.menuLink}>
-                          {item}
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
+                <ul className={styles.groupItems}>{listMenu(list03)}</ul>
               </div>
             </div>
           </div>
